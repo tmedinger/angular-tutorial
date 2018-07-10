492db06babc5bdb0ac124dd738588af5830adfd7
Tony <tmedinger@gmail.com>
2018-07-10 11:01:15 -0400

    this is already a repository?

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero"
+
+export const HEROES: Hero[] = [
+    { id: 11, name: "Mr. Nice (this name is terrible)" },
+    { id: 12, name: "Narco (this one is better but not great)" },
+    { id: 13, name: "Bombasto (this one is also terrible)" },
+    { id: 14, name: "Celeritas (bad)" },
+    { id: 15, name: "Magneta (this one's pretty good)" },
+    { id: 16, name: "RubberMan (so-so)" },
+    { id: 17, name: "Dynama (ok)" },
+    { id: 18, name: "Dr IQ (good for an annoyingly over confident smart hero)" },
+    { id: 19, name: "Magma (good one)" },
+    { id: 20, name: "Tornado (also a good one)" }
+];
